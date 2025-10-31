0ef696d8e82a59dd5c76b19648df622021a2abc5
Prashant Choudhary <get.prashant.007@gmail.com>
2025-11-01 00:57:13 +0530

    Zoneless

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { NgxLinkifyjsService, NgxLinkifyjsConfigToken, DEFAULT_CONFIG } from 'ngx-linkifyjs-v2';
 
@@ -7,7 +7,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     NgxLinkifyjsService,
     {
