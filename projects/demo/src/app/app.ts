611ee4b38da83faa736060a2896a0b04708f9a3a
Prashant Choudhary <get.prashant.007@gmail.com>
2025-12-26 17:23:35 +0530

    Migrated to latest angular

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -1,28 +1,27 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgxLinkifyjsPipe, NgxLinkifyjsService, NgxLinkifyOptions, Link } from 'ngx-linkifyjs-v2';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, FormsModule, NgxLinkifyjsPipe],
+  imports: [FormsModule, NgxLinkifyjsPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   title = 'ngx-linkifyjs Demo';
-  
+
   // Demo text samples
   sampleText = 'Check out https://github.com/code-name-jack/ngx-linkifyjs-v2 and email us at support@example.com. Follow @angular and use #ngx for hashtags!';
-  
+
   customText = 'Visit example.com or contact info@test.com';
-  
+
   // Options
   linkifyOptions: NgxLinkifyOptions = {
     className: 'custom-link',
     target: { url: '_blank' }
   };
-  
+
   // Results from service
   foundLinks: Link[] = [];
   private readonly linkifyService = inject(NgxLinkifyjsService);
@@ -30,11 +29,11 @@ export class App {
   constructor() {
     this.findLinks();
   }
-  
+
   findLinks() {
     this.foundLinks = this.linkifyService.find(this.customText);
   }
-  
+
   testLink(value: string): boolean {
     return this.linkifyService.test(value);
   }
